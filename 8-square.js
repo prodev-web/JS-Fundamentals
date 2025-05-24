50930f09fc9884ff647650f998b3b89a67380c56
prodev-web <brahim.lafrik@gmail.com>
2025-05-24 21:25:11 +0100

    Print a square of size x using X with input validation and loop, no var

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const {argv} = require('node:process');
+if(Number.isNaN(Number(argv[2]))){
+console.log('Missing size');
+}
+else{
+for (let size = 0; size < Number(argv[2]); size++){
+console.log('x'.repeat(Number(argv[2])));
+}
+}
